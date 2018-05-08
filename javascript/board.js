ff25b6b8d91d8ff12ca62288b83ce90c72c5eaec
manudebuck <!Nq32jL-Hgj-a>
2018-05-08 12:49:58 +0200

    Started js + pull script on webserver

--- javascript/board.js
+++ javascript/board.js
@@ -18,14 +18,38 @@ Function.prototype.method = function(name, func) {
 class Spelbord {
 
     constructor(args){
+        if(args.grootte < 2){
+            throw {
+                name: "AssertionError",
+                message: "Bord moet ten minste grotte 2 hebben"
+            }
+        }
         this.grootte = args.grootte;
 
         this.selector = args.selector;
+
+        this.$spelbord = $(this.selector);
 
         this.createGrid()
     }
 
     createGrid(){
+
+        let druppels = $.get('http://localhost:8000/cgi-bin/script.py',{'ACTIE' : "new", 'GROOTTE' : this.grootte.toString()}); // opvragen van cgi script
+        console.log(druppels);
+        /* const rooster = druppels.map((druppel, index) =>  {
+            return `<div class="druppel" data-kleur = "${druppel}"></div>` + ((index % this.grootte === 0) ? "</tr><tr>" : "");
+        });
+
+        this.$spelbord.html("<table><tr>" + rooster + "</tr></table>"); */
+
+        // $(".druppel", this.$spelbord).click(this.klikDruppel.bind(this));
+
+    }
+    
+    klikDruppel(event){
+
+        // do something
 
     }
 

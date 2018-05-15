7b314c879eafbaf22f9f41737b3683af1a3d8ae3
manudebuck <!Nq32jL-Hgj-a>
2018-05-16 00:05:05 +0200

    Updated cgi-file to correct game

--- cgi-bin/script.py
+++ cgi-bin/script.py
@@ -5,12 +5,24 @@ import random
 import ast
 import json
 
+greetings = ["Hello, sunshine!", "Howdy  partner!", "Hey, howdy hi!", "What's kickin', little chicken?", "Peek-a-boo!",
+             "Howdy-doody!", "My name's Ralph, and I'm a bad guy.", "Hi  mister!", "I come in peace!",
+             "Put that cookie down!", "Ahoy, matey!", "Hiya!", "'Ello, gov'nor!", "Top of the mornin' to ya!",
+             "What's crackin'?", "GOOOOOD MORNING  VIETNAM!", "'Sup  homeslice?",
+             "This call may be recorded for training purposes.", "Howdy, howdy, howdy!",
+             "Hi, my name is Lion. Pleased to eat you.", "I'm Batman.",
+             "At least  we meet for the first time for the last time!", "Hello, who's there, I'm talking.",
+             "Here's Johnny!", "You know who this is.", "Ghostbusters, whatya want?", "Yo!", "Whaddup.",
+             "Greetings and salutations!", "Hellon it's me."]
+
+
 def new_game(size=5):
     board = getRandomBoard(size)
-    moves = getPossibleMoves(board)
+    inhoud = [[0, 0]]
+    moves = getPossibleMoves(board, inhoud)
     score = 0
     message = ""
-    return {'board': board, 'moves': moves, 'score': score, 'message': message}
+    return {'board': board, 'moves': moves, 'score': score, 'message': message, 'inhoud': inhoud}
 
 
 def getRandomBoard(size=5):
@@ -31,7 +43,7 @@ def getRandomBoard(size=5):
             assert size >= 0, "Ongeldige grootte van het spelbord"
         AssertionError: Ongeldige grootte van het spelbord
         """
-    assert size >= 0, "Ongeldige grootte van het spelbord"
+    assert size > 0, "Ongeldige grootte van het spelbord"
     with open("colors.txt", "r") as file:
         kleuren = file.read().splitlines()
     kleuren = random.sample(kleuren, size)
@@ -51,50 +63,39 @@ def getPossibleMoves(board):
     >>> all(c in getPossibleMoves([['orange', 'orange', 'red'], ['green', 'blue', 'red'], ['green', 'blue', 'yellow']]) for c in {'yellow', 'orange', 'red', 'blue', 'green', 'purple'})
     False
     >>> getPossibleMoves([])
-    Traceback (most recent call last):
-      File "/Applications/PyCharm.app/Contents/helpers/pycharm/docrunner.py", line 140, in __run
-        compileflags, 1), test.globs)
-      File "<doctest cgiscript.getPossibleMoves[1]>", line 1, in <module>
-        getPossibleMoves([])
-      File "/Users/manudebuck/PyCharmProjects/ProjectSTCGI/cgiscript.py", line 56, in getPossibleMoves
-        assert len(board) > 0, "leeg board meegegeven"
-    AssertionError: leeg board meegegeven
+    []
     >>> getPossibleMoves([["red", "red"],["red", "red"]])
     ['red']
     """
-    assert len(board) > 0, "leeg board meegegeven"
     colors = set()
     for row in board:
         for color in row:
             colors.add(color)
-    colors.remove(board[0][0])
     return list(colors)
 
 
+def geldigCoordinaat(rij, kolom, bord):
+    return len(bord) > rij >= 0 and len(bord[rij]) > kolom >= 0
+
 def do_move(board, kleur, locatie):
     """
-    >>> all(kleur in do_move({"board":[["red","blue","green"],["red","blue","green"],["red","red","green"]], "moves": ["red", "blue", "green"], "score": 3, "message": ""}, "blue", [0,0])["moves"] for kleur in ["blue", "green"])
+    >>> all(kleur in do_move({"board":[["red","blue","green"],["red","blue","green"],["red","red","green"]], "moves": ["red", "blue", "green"], "score": 3, "message": ""}, "blue", [0,0])["moves"] for kleur in ["green"])
     True
-    >>> do_move({"board":[["red","blue","green"],["red","blue","green"],["red","red","green"]], "moves": ["red", "blue", "green"], "score": 3, "message": ""}, "blue", [0,0])["board"]
-    [['blue', 'blue', 'green'], ['blue', 'blue', 'green'], ['blue', 'blue', 'green']]
     >>> len(do_move({'board': [['blue', 'blue', 'green'], ['blue', 'blue', 'green'], ['blue', 'blue', 'green']], 'moves': ['green', 'blue'], 'score': 4, 'message': ''}, "green", [0,0])["message"]) > 0
     True
     >>> len(do_move({'board': [['blue', 'blue', 'green'], ['blue', 'red', 'green'], ['blue', 'blue', 'green']], 'moves': ['green', 'blue'], 'score': 4, 'message': ''}, "green", [0,0])["message"]) > 0
     False
     """
-    greetings = ["Hello, sunshine!","Howdy  partner!","Hey, howdy hi!","What's kickin', little chicken?","Peek-a-boo!","Howdy-doody!","My name's Ralph, and I'm a bad guy.","Hi  mister!","I come in peace!","Put that cookie down!","Ahoy, matey!","Hiya!","'Ello, gov'nor!","Top of the mornin' to ya!","What's crackin'?","GOOOOOD MORNING  VIETNAM!","'Sup  homeslice?","This call may be recorded for training purposes.","Howdy, howdy, howdy!","Hi, my name is Lion. Pleased to eat you.","I'm Batman.","At least  we meet for the first time for the last time!","Hello, who's there, I'm talking.","Here's Johnny!","You know who this is.","Ghostbusters, whatya want?","Yo!","Whaddup.","Greetings and salutations!","Hellon it's me."]
-    locatie = [int(locatie[0]), int(locatie[1])]
-    score = board["score"] + 1
     bordvoorstelling = board["board"]
-    assert kleur in board["moves"], "Kleur kan niet gespeeld worden"
-    assert score > 0, "ongeldige score"
-    bordvoorstelling = doeDruppel(bordvoorstelling, kleur, locatie)
+    score = board["score"]
+    bordvoorstelling = doeDruppel(bordvoorstelling, kleur, locatie, score)
     moves = getPossibleMoves(bordvoorstelling)
     message = ""
+    score = board["score"] + 1
     if(checkFinished(bordvoorstelling)):
-        message = greetings[random.randint(0, len(greetings) - 1)] + "\n \n" + \
+        message = greetings[random.randint(0, len(greetings) - 1)] + \
                   "You finished the game in: " + str(score) + " rounds!"
-    return {'board': bordvoorstelling, 'moves': moves, 'score': score, 'message': message}
+    return {'board': board, 'moves': moves, 'score': score, 'message': message}
 
 
 def checkFinished(bordvoorstelling):
@@ -113,22 +114,23 @@ def checkFinished(bordvoorstelling):
     return True
 
 
-def doeDruppel(bordvoorstelling, kleur, locatie):
+def doeDruppel(bordvoorstelling, kleur, locatie, score):
     """
-    >>> doeDruppel([[1,2,3],[1,2,2],[1,1,3]], 2, [0,0])
+    >>> doeDruppel([[1,2,3],[1,2,2],[1,1,3]], 2, [0,0], 2)
     [[2, 2, 3], [2, 2, 2], [2, 2, 3]]
-    >>> doeDruppel([[2, 2, 3], [2, 2, 2], [2, 2, 3]], 3, [0,0])
+    >>> doeDruppel([[2, 2, 3], [2, 2, 2], [2, 2, 3]], 3, [0,0], 3)
     [[3, 3, 3], [3, 3, 3], [3, 3, 3]]
-    >>> doeDruppel([[1,2,3,4],[5,4,3,2],[1,1,1,1],[2,3,1,4]], 2, [0,0])
+    >>> doeDruppel([[1,2,3,4],[5,4,3,2],[1,1,1,1],[2,3,1,4]], 2, [0,0], 0)
     [[2, 2, 3, 4], [5, 4, 3, 2], [1, 1, 1, 1], [2, 3, 1, 4]]
-    >>> doeDruppel([[2, 2, 3, 4], [5, 4, 3, 2], [1, 1, 1, 1], [2, 3, 1, 4]], 3, [0,0])
+    >>> doeDruppel([[2, 2, 3, 4], [5, 4, 3, 2], [1, 1, 1, 1], [2, 3, 1, 4]], 3, [0,0], 2)
     [[3, 3, 3, 4], [5, 4, 3, 2], [1, 1, 1, 1], [2, 3, 1, 4]]
-    >>> doeDruppel([[3, 3, 3, 4], [5, 4, 3, 2], [1, 1, 1, 1], [2, 3, 1, 4]], 1, [0,0])
+    >>> doeDruppel([[3, 3, 3, 4], [5, 4, 3, 2], [1, 1, 1, 1], [2, 3, 1, 4]], 1, [0,0], 3)
     [[1, 1, 1, 4], [5, 4, 1, 2], [1, 1, 1, 1], [2, 3, 1, 4]]
     """
     huidigekleur = bordvoorstelling[locatie[0]][locatie[1]]
     bordvoorstelling[locatie[0]][locatie[1]] = kleur
-    recDruppel(bordvoorstelling, huidigekleur, kleur, locatie, [])
+    if(score > 1):
+        recDruppel(bordvoorstelling, huidigekleur, kleur, locatie, [])
     return bordvoorstelling
 
 
